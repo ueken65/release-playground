9a1e76beb0db86fc4f202750eb1b76ec809ca543
ueken65 <ueken6522@gmail.com>
2024-08-21 19:57:38 +0900

    generate release

---
+++ script/release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "git の差分があるため処理を中断します"
+  exit 1
+fi
+
+read -p "リリースしたいバージョンを入力してください (例: 2.1.0): " version
+
+if [[ ! $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "バージョン番号は x.y.z の形式で指定してください"
+  exit 1
+fi
+
+git checkout main
+git pull
+git checkout -b "release/$version"
+
+# yaml の info.version の番号を更新
+sed -i '' -e "s/^  version: .*/  version: $version/" openapi.yaml
+
+git add openapi.yaml
+git commit -m "version $version"
+git tag $version
